d83becdcc6462dbb64d7f3ed508130efbe7d6e6a
Annmon James <annmonjameschiramel@gmail.com>
2024-04-08 16:56:19 +0530

    File names entered in that file name row for all excel files

--- Automationdashboard/CodeForAutomation24AndPowerParameters_excel_6.py
+++ Automationdashboard/CodeForAutomation24AndPowerParameters_excel_6.py
@@ -8,7 +8,7 @@ def prepare_sheet_in_memory(file_path):
     sheet.insert_rows(1)
     sheet['A1'] = 'file name'
     sheet['B1'] = file_name
-    return sheet
+    return sheet, file_name
 
 def sheet_to_dict(sheet):
     data_dict = {}
@@ -20,13 +20,15 @@ def sheet_to_dict(sheet):
 def merge_dicts(dict1, dict2):
     for key, values in dict2.items():
         if key in dict1:
-            dict1[key] += values
+            # Assuming that values is a list of values
+            dict1[key].extend(values)
         else:
             dict1[key] = values
     return dict1
 
 def process_directory(directory):
     merged_data = {}
+    file_names = []
     for root, dirs, files in os.walk(directory):
         for name in dirs:
             if name.startswith("B4"):
@@ -34,23 +36,25 @@ def process_directory(directory):
                 for file_name in os.listdir(subdir_path):
                     if file_name.endswith(".xlsx"):
                         file_path = os.path.join(subdir_path, file_name)
-                        sheet = prepare_sheet_in_memory(file_path)
+                        sheet, extracted_file_name = prepare_sheet_in_memory(file_path)
                         data_dict = sheet_to_dict(sheet)
                         merged_data = merge_dicts(merged_data, data_dict)
-    return merged_data
+                        if extracted_file_name not in file_names:
+                            file_names.append(extracted_file_name)
+    return merged_data, file_names
 
 def main():
     directory = r"C:\Lectrix_company\work\Git_Projects\Automationdashboard\Automationdashboard\MAIN_FOLDER\Automation_Dashboard_Batterywise\V4\D11_03_2024"
-    merged_data = process_directory(directory)
+    merged_data, file_names = process_directory(directory)
 
     merged_workbook = Workbook()
     merged_sheet = merged_workbook.active
-    headers_set = False
+
+    # Use the extracted file names for headers
+    headers = ['File name'] + file_names
+    merged_sheet.append(headers)
 
     for key, values in merged_data.items():
-        if not headers_set:
-            merged_sheet.append(['File name'] + ['Value {}'.format(i+1) for i in range(len(values))])
-            headers_set = True
         merged_sheet.append([key] + values)
 
     merged_file_path = os.path.join(directory, 'merged_analysis.xlsx')
