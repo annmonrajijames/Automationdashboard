3b5499fc8e14777fc1e57e4508e2c7e020b05065
KAMALESHKUMAR BALAMURUGAN <electrical.intern7@lectrixtech.com>
2024-04-15 11:51:28 +0530

    Changed the code to find the nearest t2 and added code snippets to reverse the data in the excel

--- BatteryDataSplit.py
+++ BatteryDataSplit.py
@@ -1,13 +1,24 @@
 import pandas as pd
 
 # Assuming 'df' is your dataframe with SOC data
-df = pd.read_csv(r'C:\Users\kamalesh.kb\CodeForAutomation\Automationdashboard\Automationdashboard\MAIN_FOLDER\Automation_Dashboard_Batterywise\V2\D11_03_2024_MulBattery\B4_17.33_21.40\log_file.csv')
+df = pd.read_csv(r'C:\Users\kamalesh.kb\CodeForAutomation\Automationdashboard\Automationdashboard\MAIN_FOLDER\Automation_Dashboard_Batterywise\V2\D11_03_2024_MulBattery\B3_wholeDayBB\b3_11_03_24.csv',low_memory=False)
+df['localtime'] = pd.to_datetime(df['localtime'], format=r'%d-%m-%Y %H:%M:%S')
+
+# Reverse the order of the rows
+df_reversed = df[::-1]
+# Write the reversed DataFrame to a new CSV file
+df_reversed.to_csv("reversed_csv_file.csv", index=False)
+
+# Convert 'SOC_8' column to numeric
+df_reversed['SOC_8'] = pd.to_numeric(df_reversed['SOC_8'], errors='coerce')  # 'coerce' will convert non-numeric values to NaN
 
 # Set the threshold for detecting battery change
 threshold = 40  # Example threshold value, adjust as needed
 
 # Convert 'localtime' column to datetime objects
-df['localtime'] = pd.to_datetime(df['localtime'])
+df_reversed['localtime'] = pd.to_datetime(df_reversed['localtime'], format=r'%d-%m-%Y %H:%M:%S')
+# df_reversed['localtime'] = pd.to_datetime(df_reversed['localtime'], format=r'%y-%m-%d %H:%M:%S')
+
 
 # Initialize battery_end_index and battery_number
 battery_end_index = 0
@@ -15,61 +26,68 @@ battery_number = 1
 anomaly_window= 40
 Time_window=60
 
+#Got1BatterySplit
 
 
 # Start from the rightmost end of the dataframe
-i = len(df) - 1
+i = len(df_reversed) - 1
 # Initialize the index of the last battery change
-last_battery_change_index = len(df) - 1
+last_battery_change_index = len(df_reversed) - 1
 
 t2_index=1      #to avoid the while loop enter into the infinite loop
 
 # Iterate over the dataframe in steps of 60 seconds
 while i >= 0 and t2_index >0:
-    t1 = df.iloc[i]['localtime']
+    t1 = df_reversed.iloc[i]['localtime']
     t2 = t1 + pd.Timedelta(seconds=Time_window)  # t2 is 60 seconds earlier than t1
+    print("t1------------------>",t1)
+    print("t2------------------>",t2)
     # print("t1---------->: ",t1,"t2---------->: ",t2)
 
     # Find the index of t2
-    t2_index = df[df['localtime'] <= t2].index.min()
+    t2_index = df_reversed[df_reversed['localtime'] == t2].index.min()
     
     print(i,t2_index)
     # If t2_index is NaN, find the nearest index to t2
     if pd.isna(t2_index):
-        nearest_index = (df['localtime'] - t2).abs().idxmin()
-        t2 = df.iloc[nearest_index]['localtime']
-        t2_index = nearest_index
+        print("t2 not available in the dataframe.")
+        # nearest_index = (df_reversed['localtime'] - t2).abs().idxmax
+        # t2 = df_reversed.iloc[nearest_index]['localtime']
+        # t2_index = nearest_index
+        nearest_time = df_reversed['localtime'].iloc[(df_reversed['localtime'] - t2).abs().argsort()[:1]].values[0]
+        t2 = nearest_time
+        t2_index = df_reversed[df_reversed['localtime'] == t2].index.min()
        
 
     # print(df.iloc[i]['localtime'],df.iloc[t2_index]['localtime'])    
 
     # Check if SOC difference is greater than the threshold
-    print(df.iloc[i]['SOC_8'] - df.iloc[t2_index]['SOC_8'])
+    print(df_reversed.iloc[i]['SOC_8'] - df_reversed.iloc[t2_index]['SOC_8'])
     
-    if abs(df.iloc[i]['SOC_8'] - df.iloc[t2_index]['SOC_8']) > threshold:
+    if abs(df_reversed.iloc[i]['SOC_8'] - df_reversed.iloc[t2_index]['SOC_8']) > threshold:
         print("Entered if---------->")
         # Print the time when battery change occurred (using t1 as it's the most recent timestamp)
         print("Battery changed at:", t1)
 
         start_time = t1 - pd.Timedelta(seconds=anomaly_window)
-        start_time_index = df[df['localtime'] <= start_time].index.min()
+        start_time_index = df_reversed[df_reversed['localtime'] <= start_time].index.min()
         # If t2_index is NaN, find the nearest index to t2
         if pd.isna(start_time_index):
-                nearest_index = (df['localtime'] - start_time).abs().idxmin()
-                start_time= df.iloc[nearest_index]['localtime']
+                nearest_index = (df_reversed['localtime'] - start_time).abs().idxmin()
+                start_time= df_reversed.iloc[nearest_index]['localtime']
                 start_time_index = nearest_index        
 
         
-        last_battery_change_time = df.iloc[last_battery_change_index]['localtime']
+        last_battery_change_time = df_reversed.iloc[last_battery_change_index]['localtime']
         end_time = last_battery_change_time - pd.Timedelta(seconds=anomaly_window)
-        end_time_index = df[df['localtime'] <= end_time].index.min()
+        end_time_index = df_reversed[df_reversed['localtime'] <= end_time].index.min()
         # If t2_index is NaN, find the nearest index to t2
         if pd.isna(end_time_index):
-                nearest_index = (df['localtime'] - end_time).abs().idxmin()
-                end_time= df.iloc[nearest_index]['localtime']
+                nearest_index = (df_reversed['localtime'] - end_time).abs().idxmin()
+                end_time= df_reversed.iloc[nearest_index]['localtime']
                 end_time_index = nearest_index
 
-        battery_data = df.iloc[start_time_index:end_time_index].copy()  # Data from the last battery change to the current battery change
+        battery_data = df_reversed.iloc[start_time_index:end_time_index].copy()  # Data from the last battery change to the current battery change
 
 
                     #Create a new dataframe for each battery within the current window
