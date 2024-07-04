ab2dbd0011d70ff295296e10d07cf8d25a7f059e
Srijanani B <srijanani@LTPL.local>
2024-07-04 14:50:18 +0530

    Changed the allignment

--- master/main/GUI2.py
+++ master/main/GUI2.py
@@ -54,7 +54,7 @@ scripts = {
 }
 dropdown = tk.OptionMenu(app, file_var, *scripts.values())
 dropdown.config(bg="lightblue", fg="black")
-dropdown.grid(row=0, column=1, padx=10, pady=10)
+dropdown.grid(row=0, column=1, padx=10, pady=10, sticky='w')  # Align to the left (west)
 
 # Label for displaying the folder path
 path_label = tk.Label(app, text="Choose to see input folder path", bg="lightblue", fg="black")
@@ -62,7 +62,7 @@ path_label.grid(row=1, column=0, padx=10, pady=10, sticky='e')
 
 # Button to choose the folder
 folder_button = tk.Button(app, text="Choose Folder", command=open_folder, bg="lightblue", fg="black")
-folder_button.grid(row=1, column=1, padx=10, pady=10)
+folder_button.grid(row=1, column=1, padx=10, pady=10, sticky='w')  # Align to the left (west)
 
 # Label for the "Save output in preferred location" checkbox
 save_output_label = tk.Label(app, text="Run to see the output folder path", bg="lightblue", fg="black")
@@ -71,7 +71,7 @@ save_output_label.grid(row=2, column=0, padx=10, pady=5, sticky='e')
 # Checkbox for copying the folder
 copy_var = tk.BooleanVar()
 copy_check = tk.Checkbutton(app, text="Save output in preferred location", variable=copy_var, bg="lightblue", fg="black")
-copy_check.grid(row=2, column=1, padx=10, pady=5)
+copy_check.grid(row=2, column=1, padx=10, pady=5, sticky='w')  # Align to the left (west)
 
 # Button to run the script
 run_button = tk.Button(app, text="Run", command=run_script, bg="lightblue", fg="black")
